3c5285b279cef334ace3c6cc7f9e0b4d712443d5
Ethan Rickert <ethan@Ethans-MacBook-Pro.local>
2022-01-26 23:02:19 -0500

    implemented skeleton auth command

--- bot.py
+++ bot.py
@@ -1,16 +1,26 @@
 # bot.py
 import os
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='!')
 
-client = discord.Client()
+# acccesses bot email credentials used for user authentication
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+def get_credentials():
+    return os.getenv('BOT_EMAIL_USER'), os.getenv('BOT_EMAIL_PASS')
 
-client.run(TOKEN)
+# user authentication via Discord command !auth [netid]
+
+@bot.command(name='auth')
+async def auth(ctx: commands.Context, *, netid):
+    if netid is None:
+        await ctx.send("Please enter a valid UTK NetID!")
+        return
+    response = f"{ctx.message.author.mention} Check your UTK email and enter the code received into the chat."
+    await ctx.send(response)
+
+bot.run(TOKEN)
