108cb4f0e0187fbf779ec81661374c4b863b2e8a
Ethan Rickert <ethan@Ethans-MacBook-Pro.local>
2022-01-29 08:47:58 -0500

    final changes

--- bot.py
+++ bot.py
@@ -30,14 +30,30 @@ async def send_auth_email(user, passkey):
     s.sendmail(BOT_EMAIL, str(user)+email_tag, msg)
     s.quit()
 
+# check if a key already exists
+def key_exists(user):
+    data = {} # json dict placeholder
+    if os.path.getsize('members.json') != 0: # if members.json is not empty...
+        with open('members.json') as f:      
+            data = json.load(f)              # read dict into data
+
+    with open('members.json', 'w') as f:     
+        if str(user) in data:
+            json.dump(data, f, indent=2)
+            f.close()
+            return True
+    return False
+
 # updates member list for auth tracking
 def update_members(auth_pair):
-    data = {}
-    if os.path.getsize('members.json') != 0:
-        with open('members.json') as f:
-            data = json.load(f)
-    with open('members.json', 'w') as f:
+    data = {} # json dict placeholder
+    if os.path.getsize('members.json') != 0: # if members.json is not empty...
+        with open('members.json') as f:      
+            data = json.load(f)              # read dict into data
+
+    with open('members.json', 'w') as f:     
         data[auth_pair[0]] = auth_pair[1]
+        print(data)
         json.dump(data, f, indent=2)
 
 def remove_member(user):
@@ -52,22 +68,24 @@ def remove_member(user):
 # return passkey mapped to user ID from members.json
 async def get_auth_pair(user):
     f = open('members.json')
+    print(f'data[{str(user)}]')
     data = json.load(f)
-    for key, value in data.items():
-        if str(key) == str(user):
-            f.close()
-            return str(value)
     f.close()
+    print(data)
+    if str(user) in data:
+        return data[str(user)]
     return None
 
 # check is user is already authenticated (has Student role)
 def is_auth(user):
+    print('checking authorization')
     roles = [str(role) for role in user.roles]
     if auth_role in roles:
         print(f'{user} is already authenticated!')
         return True
     return False
 
+# add auth role to user
 async def assign_role(user):
     if is_auth(user):
         return
@@ -85,33 +103,42 @@ async def auth(ctx: commands.Context, *, netid=None):
     if is_auth(member):
         return
     if netid is None:
-        response = f"{member.mention} Please enter a valid UTK NetID!"
-        await ctx.send(response)
+        await ctx.send(f"{member.mention} Please enter a valid UTK NetID!")
+        return
+    if key_exists(member.id):
+        await ctx.send(f'{member.mention} there already exists a passcode for this NetID. Type `!reset` to reset your passcode.')
         return
     
     passkey = ''.join([str(number) for number in np.random.randint(10, size=6)]) # generate passkey
     auth_pair = (member.id, passkey)                                             # serialize ID & key
     update_members(auth_pair)
-    await send_auth_email(netid, passkey)                                        # send auth email
+    await send_auth_email(netid, passkey)                                       # send auth email
     response = f"{member.mention} Check your UTK email and enter the code received into the chat."
     await ctx.send(response)
 
 
 # read passkey for valid match
-@bot.event
-async def on_message(message):
-    member = message.author
-    if (member.id == bot.user.id) or (str(message.channel) != auth_channel):
-        return
-    await bot.process_commands(message)
+@bot.command(name='verify')
+async def verify(ctx: commands.Context, *, passkey: str):
+    member = ctx.message.author
     if is_auth(member):
         return
 
     # if passkey matches members.json, assign Student role and confirm authentication
-    if str(message.content) == await get_auth_pair(member.id):
-        await message.channel.send(f"{member.mention} Authentication succesful!")
+    if passkey == await get_auth_pair(member.id):
+        await ctx.message.channel.send(f"{member.mention} Authentication succesful!")
         await assign_role(member)
         remove_member(member.id)
+    return
+
+# resets passkey if it already exists in members.json
+@bot.command(name='reset')
+async def reset(ctx: commands.Context):
+    if is_auth(ctx.message.author):
+        await ctx.send(f'{ctx.message.author.mention} you\'re already authorized!')
+        return
+    remove_member(ctx.message.author.id)
+    await ctx.send(f'{ctx.message.author.mention} your passcode has been reset!')
     return
 
 bot.run(TOKEN)
