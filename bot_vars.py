d1c11954e472adaaf1114fabb2306f820861aa07
ethan0429 <ethanrickert0429@gmail.com>
2022-08-30 13:15:16 -0400

    updated course ID for 2022 semester

--- bot_vars.py
+++ bot_vars.py
@@ -33,7 +33,7 @@ API_URL = 'https://utk.instructure.com/'
 # Canvas API key
 API_KEY = os.getenv('CANVAS_KEY')
 canvas = Canvas(API_URL, API_KEY)
-CONST_COSC102_COURSE_ID = 139798
+CONST_COSC102_COURSE_ID = 154574
 users = {}
 
 AuthID = namedtuple('AuthID', ['member_id', 'passkey', 'netid'])
