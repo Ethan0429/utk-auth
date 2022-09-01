f95fa7164e217e9574dafa87f8acd243f0a3fbb0
Ethan Rickert <ethanrickert0429@gmail.com>
2022-09-01 17:35:05 -0400

    updated for 102

--- bot_vars.py
+++ bot_vars.py
@@ -2,30 +2,12 @@ import os
 from canvasapi import Canvas
 from collections import namedtuple
 
-# if using dotenv, un-comment:
-
-# if os.path.exists('.env'):
-#     pass
-# else:
-#     with open('.env', 'w') as f:
-#         f.writelines([
-#             '# .env\n',
-#             'DISCORD_TOKEN=\n',
-#             'BOT_EMAIL_USER=\n'
-#             'BOT_EMAIL_PASS=\n',
-#             'CANVAS_KEY=\n'
-#         ])
-#     print('.env file generated! Exiting bot script.\nFill the appropriate fields in the .env file before running the script again!')
-#     sys.exit()
-
-# load_dotenv()
-
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 print(f"Discord bot token: {TOKEN}\n")
 
 auth_channel = 'auth'
-auth_role    = 'COSC 101'
+auth_role    = 'COSC 102'
 email_tag    = '@vols.utk.edu'
 
 # Canvas API URL
@@ -33,7 +15,7 @@ API_URL = 'https://utk.instructure.com/'
 # Canvas API key
 API_KEY = os.getenv('CANVAS_KEY')
 canvas = Canvas(API_URL, API_KEY)
-CONST_COSC101_COURSE_ID = 154564
+CONST_COSC102_COURSE_ID = 154574
 users = {}
 
 AuthID = namedtuple('AuthID', ['member_id', 'passkey', 'netid'])
