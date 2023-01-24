7a8b10e42a82cb822aa6cf0e0d2920a37df1003c
Ethan Rickert <ethanrickert0429@gmail.com>
2023-01-24 07:56:56 -0500

    updating canvas course ID

--- bot_vars.py
+++ bot_vars.py
@@ -9,8 +9,8 @@ from collections import namedtuple
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 auth_channel = 'auth'
-auth_role    = 'COSC 102'
-email_tag    = '@vols.utk.edu'
+auth_role = 'COSC 102'
+email_tag = '@vols.utk.edu'
 
 # Canvas API URL
 API_URL = 'https://utk.instructure.com/'
@@ -19,7 +19,7 @@ API_KEY = os.getenv('CANVAS_KEY')
 canvas = Canvas(API_URL, API_KEY)
 CONST_COSC102_GUILD_ID = 935991929978621962
 CONST_COSC102_GRADER_ROLE_ID = 943277040445038612
-CONST_COSC102_COURSE_ID = 154574
+CONST_COSC102_COURSE_ID = 164563
 CONST_COSC102_GRADING_CHANNEL_ID = 1017518807654346853
 users = {}
 
